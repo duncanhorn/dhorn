7d89abdf7195b5056311cefabd1c4080f441b11d
Duncan Horn <duncan.horn@utexas.edu>
2015-09-10 03:29:18 -0700

    Got unit_cast working

--- include/dhorn/units.h
+++ include/dhorn/units.h
@@ -14,96 +14,22 @@
 namespace dhorn
 {
     /*
-     * Unit types
-     */
-    enum class unit_type
-    {
-        length      = 1,
-        area        = 2,
-        volume      = 3,
-        mass        = 4,
-        time        = 5,
-        current     = 6,
-        temperature = 7,
-        quantity    = 8,
-        luminosity  = 9,
-    };
-
-
-
-    /*
      * Conversion helpers (squared, cubed, etc.)
      */
     namespace garbage
     {
         template <typename Ty, size_t Power>
-        struct power
+        struct unit_power
         {
             static_assert(Power != 0, "Power of zero is not supported");
 
+            using type = Ty;
+
+            // TODO: Due to new naming, give this function a name
             inline constexpr Ty operator()(_In_ const Ty &val) const
             {
-                return val * ((Power == 1) ? 1 : power<Power - 1>(val));
+                return val * ((Power == 1) ? 1 : unit_power<Power - 1>(val));
             }
-        };
-
-
-
-        template <unit_type Type, typename Ty>
-        struct unit_traits;
-
-        template <typename Ty>
-        struct unit_traits<unit_type::length, Ty>
-        {
-            using convert_type = power<Ty, 1>;
-        };
-
-        template <typename Ty>
-        struct unit_traits<unit_type::area, Ty>
-        {
-            using convert_type = power<Ty, 2>;
-        };
-
-        template <typename Ty>
-        struct unit_traits<unit_type::volume, Ty>
-        {
-            using convert_type = power<Ty, 3>;
-        };
-
-        template <typename Ty>
-        struct unit_traits<unit_type::mass, Ty>
-        {
-            using convert_type = power<Ty, 1>;
-        };
-
-        template <typename Ty>
-        struct unit_traits<unit_type::time, Ty>
-        {
-            using convert_type = power<Ty, 1>;
-        };
-
-        template <typename Ty>
-        struct unit_traits<unit_type::current, Ty>
-        {
-            using convert_type = power<Ty, 1>;
-        };
-
-        template <typename Ty>
-        struct unit_traits<unit_type::temperature, Ty>
-        {
-            using convert_type = power<Ty, 1>;
-        };
-
-        template <typename Ty>
-        struct unit_traits<unit_type::quantity, Ty>
-        {
-            using convert_type = power<Ty, 1>;
-        };
-
-        template <typename Ty>
-        struct unit_traits<unit_type::luminosity, Ty>
-        {
-            using convert_type = power<Ty, 1>;
         };
     }
 
@@ -114,7 +40,7 @@ namespace dhorn
      */
 #pragma region Unit Type
 
-    template <unit_type Unit, typename Ty, typename Ratio>
+    template <typename UnitType, typename Ratio>
     class unit;
 
     // Helpers
@@ -132,36 +58,137 @@ namespace dhorn
 
 
 
+        template <typename Ty>
+        struct unit_traits
+        {
+            static const bool is_unit = false;
+        };
+
+        template <typename UnitType, typename Ratio>
+        struct unit_traits<unit<UnitType, Ratio>>
+        {
+            static const bool is_unit = true;
+            using unit_type = UnitType;
+            using ratio_type = Ratio;
+        };
+
+
+
         template <typename Lhs, typename Rhs>
         struct unit_result_type;
 
-        template <unit_type Unit, typename Ty, typename Ratio, typename Rhs>
-        struct unit_result_type<unit<Unit, Ty, Ratio>, Rhs>
+        template <typename UnitType1, typename Ratio1, typename UnitType2, typename Ratio2>
+        struct unit_result_type<unit<UnitType1, Ratio1>, unit<UnitType2, Ratio2>>
         {
-            using type = unit<Unit, typename std::common_type<Ty, Rhs>::type, Ratio>;
+        private:
+            // The result of arithmetic operation op(val1, val2)
+            //      val1 = x * (n1 / d1)
+            //      val2 = y * (n2 / d2)
+            // is going to have a resulting ratio of type std::ratio<n', d'> where
+            //      n' = GCF(n1 * d2, n2 * d1)
+            //      d' = d1 * d2
+            //  and a resulting value of
+            //      op(x * n1 * d2, y * n2 * d1) / n'
+            //
+            // Note the candidates for integer overflow:
+            //      n1 * d2
+            //      n2 * d1
+            //      d1 * d2
+            //
+            // The last one (d1 * d2) is particularly worrisome in cases where both units are very "small" (think
+            // adding picometers to picometers). The other two aren't as worrisome (since adding gigameters to
+            // picometers will almost certainly overflow in units of picometers), but we should still give the correct
+            // ratio for the result type and instead let the overflow happen when the operation takes place. E.g. if
+            // we are adding zero gigameters to 10 picometers, we would expect no overflow to take place. To accomplish
+            // this, we need to separate numerators and denominators out so that we can deal with and reduce each
+            // separately
+            using Num1 = std::ratio<Ratio1::num, 1>;
+            using Num2 = std::ratio<Ratio2::num, 1>;
+            using Den1 = std::ratio<1, Ratio1::den>;
+            using Den2 = std::ratio<1, Ratio2::den>;
+
+            using DenRatio1 = std::ratio_divide<Den1, Den2>;
+            using DenRatio2 = std::ratio_divide<Den2, Den1>;
+
+
+
+
+            // Attempt to reduce the denominator of each ratio. Note that if the numerator of one is very large and
+            // the denominator of the other is very large, we will likely have integer overflow, but even if we were
+            // able to avoid it here, the final result would more than likely have the same issue
+            using Den1Ratio = std::ratio<1, Ratio1::den>;
+            using Den2Ratio = std::ratio<1, Ratio2::den>;
+
+
+
+
+
+
+            using Unit1 = unit<UnitType1, Ratio1>;
+            using Unit2 = unit<UnitType2, Ratio2>;
+
+            using ValueType1 = typename UnitType1::type;
+            using ValueType2 = typename UnitType2::type;
+            using CommonValueType = typename std::common_type<ValueType1, ValueType2>::type;
+
+        public:
+            // TODO: using type = unit<UnitType, typename std::common_type<Ty, Rhs>::type, Ratio>;
         };
     }
 
 
 
-    template <unit_type Unit, typename Ty, typename Ratio>
+    /*
+     * Unit types
+     */
+#pragma region Unit Types
+
+    namespace unit_type
+    {
+        template <typename Ty>
+        struct length : public garbage::unit_power<Ty, 1> {};
+
+        template <typename Ty>
+        struct area : public garbage::unit_power<Ty, 2> {};
+
+        template <typename Ty>
+        struct volume : public garbage::unit_power<Ty, 3> {};
+
+        template <typename Ty>
+        struct mass : public garbage::unit_power<Ty, 1> {};
+
+        template <typename Ty>
+        struct time : public garbage::unit_power<Ty, 1> {};
+
+        template <typename Ty>
+        struct current : public garbage::unit_power<Ty, 1> {};
+
+        template <typename Ty>
+        struct temperature : public garbage::unit_power<Ty, 1> {};
+
+        template <typename Ty>
+        struct quantity : public garbage::unit_power<Ty, 1> {};
+
+        template <typename Ty>
+        struct luminosity : public garbage::unit_power<Ty, 1> {};
+    }
+
+#pragma endregion
+
+
+
+    // NOTE: Arithmetic operations can very easily overflow if the GCF of the denominators is small compared to the
+    // two denominators. Therefore, it is highly recommended that only powers of ten are used
+    template <typename UnitType, typename Ratio>
     class unit final
     {
         static_assert(garbage::is_ratio<Ratio>::value, "Second template argument must be of type std::ratio");
-
-        /*
-         * Private types
-         */
-        using Traits = garbage::unit_traits<Unit, Ty>;
-
-
 
     public:
         /*
          * Public types
          */
-        using value_type = typename Ty;
-        static const unit_type type = Unit;
+        using value_type = typename UnitType::type;
 
 
 
@@ -284,30 +311,30 @@ namespace dhorn
     /*
      * Value Multiplication/Division/Modulous
      */
-    template <unit_type Unit, typename Ty, typename Ratio, typename ValueType>
-    inline unit<Unit, Ty, Ratio> operator*(_In_ const unit<Unit, Ty, Ratio> &lhs, _In_ const ValueType &rhs)
+    template <typename UnitType, typename Ratio, typename ValueType>
+    inline unit<UnitType, Ratio> operator*(_In_ const unit<UnitType, Ratio> &lhs, _In_ const ValueType &rhs)
     {
-        return unit<Unit, Ty, Ratio>(lhs.value() * rhs);
+        return unit<UnitType, Ratio>(lhs.value() * rhs);
     }
 
-    template <typename ValueType, unit_type Unit, typename Ty, typename Ratio>
-    inline unit<Unit, Ty, Ratio> operator*(_In_ const ValueType &lhs, _In_ const unit<Unit, Ty, Ratio> &rhs)
+    template <typename ValueType, typename UnitType, typename Ratio>
+    inline unit<UnitType, Ratio> operator*(_In_ const ValueType &lhs, _In_ const unit<UnitType, Ratio> &rhs)
     {
-        return unit<Unit, Ty, Ratio>(lhs * rhs.value());
+        return unit<UnitType, Ratio>(lhs * rhs.value());
     }
 
-    template <unit_type Unit, typename Ty, typename Ratio, typename ValueType>
-    inline unit<Unit, Ty, Ratio> operator/(_In_ const unit<Unit, Ty, Ratio> &lhs, _In_ const ValueType &rhs)
+    template <typename UnitType, typename Ratio, typename ValueType>
+    inline unit<UnitType, Ratio> operator/(_In_ const unit<UnitType, Ratio> &lhs, _In_ const ValueType &rhs)
     {
         // Note that it does not make sense to divide a value by a unit; only a unit by a value
-        return unit<Unit, Ty, Ratio>(lhs.value() / rhs);
+        return unit<UnitType, Ratio>(lhs.value() / rhs);
     }
 
-    template <unit_type Unit, typename Ty, typename Ratio, typename ValueType>
-    inline unit<Unit, Ty, Ratio> operator%(_In_ const unit<Unit, Ty, Ratio> &lhs, _In_ const ValueType &rhs)
+    template <typename UnitType, typename Ratio, typename ValueType>
+    inline unit<UnitType, Ratio> operator%(_In_ const unit<UnitType, Ratio> &lhs, _In_ const ValueType &rhs)
     {
         // Note that it does not make sense to divide a value by a unit; only a unit by a value
-        return unit<Unit, Ty, Ratio>(lhs.value() % rhs);
+        return unit<UnitType, Ratio>(lhs.value() % rhs);
     }
 
 
@@ -332,46 +359,82 @@ namespace dhorn
     //    return unit<Unit, Ratio>(lhs) += rhs;
     //}
 
+
+
+    /*
+     * unit_cast
+     */
+    template <
+        typename TargetType,
+        typename UnitType,
+        typename Ratio,
+        typename = typename std::enable_if<garbage::unit_traits<TargetType>::is_unit>::type,
+        typename = typename std::enable_if<std::is_same<UnitType, typename garbage::unit_traits<TargetType>::unit_type>::value>::type>
+    inline constexpr TargetType unit_cast(_In_ const unit<UnitType, Ratio> &val)
+    {
+        using target_ratio = typename garbage::unit_traits<TargetType>::ratio_type;
+        using convert_ratio = std::ratio_divide<Ratio, target_ratio>;
+
+        return
+            ((convert_ratio::num == 1) && (convert_ratio::den == 1))
+            ? TargetType(val.value())
+            : (
+                (convert_ratio::den == 1)
+                ? TargetType(val.value() * convert_ratio::num)
+                : (
+                    (convert_ratio::num == 1)
+                    ? TargetType(val.value() / convert_ratio::den)
+                    : TargetType(val.value() * convert_ratio::num / convert_ratio::den)
+                )
+            );
+    }
+
 #pragma endregion
 
 
 
 #pragma region Type Definitions
 
-    enum class unit_type
-    {
-        length = 1,
-        area = 2,
-        volume = 3,
-        mass = 4,
-        time = 5,
-        current = 6,
-        temperature = 7,
-        quantity = 8,
-        luminosity = 9,
-    };
-
-    // TODO: Fix all of these ratios
     // TODO: Can we get away with not using double...?
 
     // Length; base is meter
-    using attometer     = unit<unit_type::length, intmax_t, std::atto>;
-    using femtometer    = unit<unit_type::length, intmax_t, std::femto>;
-    using picometer     = unit<unit_type::length, intmax_t, std::pico>;
-    using nanometer     = unit<unit_type::length, intmax_t, std::nano>;
-    using micrometer    = unit<unit_type::length, intmax_t, std::micro>;
-    using millimeter    = unit<unit_type::length, intmax_t, std::milli>;
-    using centimeter    = unit<unit_type::length, intmax_t, std::centi>;
-    using decimeter     = unit<unit_type::length, intmax_t, std::deci>;
-    using meter         = unit<unit_type::length, intmax_t, std::ratio<1>>;
-    using decameter     = unit<unit_type::length, intmax_t, std::deca>;
-    using hectometer    = unit<unit_type::length, intmax_t, std::hecto>;
-    using kilometer     = unit<unit_type::length, intmax_t, std::kilo>;
-    using megameter     = unit<unit_type::length, intmax_t, std::mega>;
-    using gigameter     = unit<unit_type::length, intmax_t, std::giga>;
-    using terameter     = unit<unit_type::length, intmax_t, std::tera>;
-    using petameter     = unit<unit_type::length, intmax_t, std::peta>;
-    using exameter      = unit<unit_type::length, intmax_t, std::exa>;
+#pragma region Length
+
+    // SI units
+    using attometers    = unit<unit_type::length<intmax_t>, std::atto>;
+    using femtometers   = unit<unit_type::length<intmax_t>, std::femto>;
+    using picometers    = unit<unit_type::length<intmax_t>, std::pico>;
+    using nanometers    = unit<unit_type::length<intmax_t>, std::nano>;
+    using micrometers   = unit<unit_type::length<intmax_t>, std::micro>;
+    using millimeters   = unit<unit_type::length<intmax_t>, std::milli>;
+    using centimeters   = unit<unit_type::length<intmax_t>, std::centi>;
+    using decimeters    = unit<unit_type::length<intmax_t>, std::deci>;
+    using meters        = unit<unit_type::length<intmax_t>, std::ratio<1>>;
+    using decameters    = unit<unit_type::length<intmax_t>, std::deca>;
+    using hectometers   = unit<unit_type::length<intmax_t>, std::hecto>;
+    using kilometers    = unit<unit_type::length<intmax_t>, std::kilo>;
+    using megameters    = unit<unit_type::length<intmax_t>, std::mega>;
+    using gigameters    = unit<unit_type::length<intmax_t>, std::giga>;
+    using terameters    = unit<unit_type::length<intmax_t>, std::tera>;
+    using petameters    = unit<unit_type::length<intmax_t>, std::peta>;
+    using exameters     = unit<unit_type::length<intmax_t>, std::exa>;
+
+    // TODO: Imperial
+
+#pragma endregion
+
+    //enum class unit_type
+    //{
+    //    length = 1,
+    //    area = 2,
+    //    volume = 3,
+    //    mass = 4,
+    //    time = 5,
+    //    current = 6,
+    //    temperature = 7,
+    //    quantity = 8,
+    //    luminosity = 9,
+    //};
 
 #pragma endregion
 }
--- tests/UnitsTests.cpp
+++ tests/UnitsTests.cpp
@@ -17,6 +17,118 @@ namespace dhorn
 {
     namespace tests
     {
+        TEST_CLASS(UnitsTests)
+        {
+            TEST_METHOD(PositiveTest)
+            {
+                dhorn::meters val(1);
+                Assert::IsTrue((+val).value() == 1);
 
+                val = -2;
+                Assert::IsTrue((+val).value() == -2);
+
+                val = 0;
+                Assert::IsTrue((+val).value() == 0);
+            }
+
+            TEST_METHOD(NegateTest)
+            {
+                dhorn::meters val(1);
+                Assert::IsTrue((-val).value() == -1);
+
+                val = -2;
+                Assert::IsTrue((-val).value() == 2);
+
+                val = 0;
+                Assert::IsTrue((-val).value() == 0);
+            }
+
+            TEST_METHOD(IncrementTest)
+            {
+                dhorn::meters val(0);
+
+                Assert::IsTrue((++val).value() == 1);
+                Assert::IsTrue(val.value() == 1);
+
+                Assert::IsTrue((val++).value() == 1);
+                Assert::IsTrue(val.value() == 2);
+            }
+
+            TEST_METHOD(DecrementTest)
+            {
+                dhorn::meters val(0);
+
+                Assert::IsTrue((--val).value() == -1);
+                Assert::IsTrue(val.value() == -1);
+
+                Assert::IsTrue((val--).value() == -1);
+                Assert::IsTrue(val.value() == -2);
+            }
+
+            TEST_METHOD(MultiplyTest)
+            {
+                dhorn::meters val(10);
+                Assert::IsTrue((val * 10).value() == 100);
+
+                val *= 3;
+                Assert::IsTrue(val.value() == 30);
+
+                val = -10;
+                Assert::IsTrue((val * 2).value() == -20);
+
+                val *= 4;
+                Assert::IsTrue(val.value() == -40);
+            }
+
+            TEST_METHOD(DivideTest)
+            {
+                dhorn::meters val(100);
+                Assert::IsTrue((val / 2).value() == 50);
+
+                val /= 20;
+                Assert::IsTrue(val.value() == 5);
+
+                val = -100;
+                Assert::IsTrue((val / 5).value() == -20);
+
+                val /= 50;
+                Assert::IsTrue(val.value() == -2);
+            }
+
+            TEST_METHOD(ModulousTest)
+            {
+                dhorn::meters val(10);
+                Assert::IsTrue((val % 3).value() == 1);
+
+                val %= 6;
+                Assert::IsTrue(val.value() == 4);
+
+                val = -10;
+                Assert::IsTrue((val % 4).value() == -2);
+
+                val %= 7;
+                Assert::IsTrue(val.value() == -3);
+            }
+
+            // The unit_result_type helper is key in determining the correct return type of arithmetic operations and
+            // thus deserves its own tests
+            //TEST_METHOD(UnitResultTypeTest)
+            //{
+            //}
+
+            TEST_METHOD(UnitCastTest)
+            {
+                dhorn::kilometers km(1);
+
+                // One kilometer is 1000 meters
+                auto m = dhorn::unit_cast<dhorn::meters>(km);
+                Assert::IsTrue(m.value() == 1000);
+
+                // 2500 meters is 2.5 (rounds down to 2) kilometers
+                m = dhorn::meters(2500);
+                km = dhorn::unit_cast<dhorn::kilometers>(m);
+                Assert::IsTrue(km.value() == 2);
+            }
+        };
     }
 }
