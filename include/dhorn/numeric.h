b15d876ba68d7e8a1719a883ebfba231c0860d58
Duncan Horn <duncan.horn@utexas.edu>
2015-11-04 00:26:51 -0800

    numeric.h is mostly complete. Just need to test more

--- include/dhorn/numeric.h
+++ include/dhorn/numeric.h
@@ -18,18 +18,16 @@ namespace dhorn
 {
     namespace garbage
     {
-        template <
-            typename NumericType,
-            typename = std::enable_if<std::is_signed<NumericType>::value>::type>
-        inline constexpr void negate(_In_ NumericType val)
+        template <typename NumericType>
+        inline constexpr typename std::enable_if<std::is_signed<NumericType>::value, NumericType>::type
+        negate(_In_ NumericType val)
         {
             return -val;
         }
 
-        template <
-            typename NumericType,
-            typename = std::enable_if<std::is_unsigned<NumericType>::value>::type>
-        inline constexpr void negate(_In_ NumericType val)
+        template <typename NumericType>
+        inline constexpr typename std::enable_if<std::is_unsigned<NumericType>::value, NumericType>::type
+        negate(_In_ NumericType)
         {
             throw std::invalid_argument("Cannot negate an unsigned type");
         }
@@ -38,6 +36,13 @@ namespace dhorn
         inline constexpr bool is_digit(_In_ CharT ch) noexcept
         {
             return (ch >= '0') && (ch <= '9');
+        }
+
+        template <typename CharT>
+        inline const CharT *skip_digits(_In_ const CharT *str) noexcept
+        {
+            for (; is_digit(*str); ++str);
+            return str;
         }
 
         template <typename NumericType, typename CharT>
@@ -53,9 +58,29 @@ namespace dhorn
         }
 
         template <typename CharT>
+        inline const CharT *skip_sign(_In_ CharT *str) noexcept
+        {
+            if (is_sign(*str))
+            {
+                ++str;
+            }
+            return str;
+        }
+
+        template <typename CharT>
         inline constexpr bool is_decimal(_In_ CharT ch) noexcept
         {
             return ch == '.';
+        }
+
+        template <typename CharT>
+        inline const CharT *skip_decimal(_In_ CharT *str) noexcept
+        {
+            if (is_decimal(*str))
+            {
+                ++str;
+            }
+            return str;
         }
 
         template <typename CharT>
@@ -64,16 +89,37 @@ namespace dhorn
             return (ch == 'e') || (ch == 'E');
         }
 
-        template <typename NumericType, typename CharT>
-        NumericType read_single_number(_Inout_ const CharT **str)
+        template <typename CharT>
+        inline const CharT *skip_exponential(_In_ CharT *str) noexcept
         {
-            NumericType value = {};
-            for (; is_digit(**str); ++(*str))
+            if (is_exponential(*str))
             {
-                value = (value * 10) + to_digit<NumericType>(**str);
+                ++str;
+            }
+            return str;
+        }
+
+        template <typename NumericType, typename CharT>
+        const CharT *read_single_number(_In_ const CharT *str, _Inout_ NumericType *value)
+        {
+            for (; is_digit(*str); ++str)
+            {
+                *value = ((*value) * 10) + to_digit<NumericType>(*str);
             }
 
-            return value;
+            return str;
+        }
+
+        template <typename NumericType, typename CharT>
+        const CharT *read_single_number(_In_ const CharT *front, _In_ const CharT *back, _Inout_ NumericType *value)
+        {
+            for (; front < back; ++front)
+            {
+                assert(is_digit(*front));
+                *value = ((*value) * 10) + to_digit<NumericType>(*front);
+            }
+
+            return front;
         }
     }
 
@@ -84,46 +130,115 @@ namespace dhorn
      */
 #pragma region String Literal Conversion
 
-    template <typename NumericType, typename CharT>
-    NumericType numeric_cast(_In_ const CharT *str)
-    {
-        NumericType integer_part;
-        NumericType fractional_part = {};
-        intmax_t exponent_part = 0;
+     template <typename NumericType, typename CharT>
+     NumericType numeric_cast(_In_ const CharT *str)
+     {
+         // Since the string could be of some crazy form like 10000000000000e-13, we need to keep the value in string
+         // form for as long as possible and shift the decimal via string pointers
+         const CharT *integer_start;
+         const CharT *integer_end;
+         const CharT *fraction_start;
+         const CharT *fraction_end;
+         const CharT *string_front = str;
 
-        // Check for negative value. If the type is unsigned, we'll throw later
-        bool negative = *str == static_cast<CharT>('-');
+         str = garbage::skip_sign(str);
 
-        // Skip past any sign that might be present
-        if (garbage::is_sign(*str))
-        {
-            ++str;
-        }
+         // This may yield a string of zero length, which is okay
+         integer_start = str;
+         str = garbage::skip_digits(str);
+         integer_end = str;
 
-        // Read the digits for the integer part
-        integer_part = garbage::read_single_number(&str);
+         str = garbage::skip_decimal(str);
 
-        // Negate if necessary
-        if (negative)
-        {
-            integer_part = garbage::negate(integer_part);
-        }
+         // This may yield a string of zero length, which is okay
+         fraction_start = str;
+         str = garbage::skip_digits(str);
+         fraction_end = str;
 
-        if (garbage::is_decimal(*str))
-        {
-            ++str;
+         // We initially read the exponent as intmax_t since any value whose exponent is greater than
+         // std::numeric_limits<intmax_t>::max() is guaranteed to not be representable
+         intmax_t exponent = 0;
+         if (garbage::is_exponential(*str))
+         {
+             ++str;
 
-            // Read the digits for the fractional part
-            fractional_part = garbage::read_single_number(&str);
-        }
+             bool isExponentNegative = (*str == '-');
+             str = garbage::skip_sign(str);
 
-        if (garbage::is_exponential(*str))
-        {
-            ++str;
+             str = garbage::read_single_number<intmax_t>(str, &exponent);
+             if (isExponentNegative)
+             {
+                 exponent = -exponent;
+             }
+         }
 
+         // We now read an integer part and a fractional part in the resulting part. Since we know the exponent, we can
+         // correctly shift the decimal so that if we get integer overflow we know that it is unavoidable and the
+         // undefined behavior is okay
+         NumericType integer_part = {};
+         NumericType fractional_part = {};
+         NumericType fractional_divide = 1;
 
-        }
-    }
+         if (exponent >= 0)
+         {
+             // All of the integer remains. Note that we still need to multiply by some power of 10. That will come
+             // when we read the fraction
+             garbage::read_single_number(integer_start, integer_end, &integer_part);
+
+             // Some of fraction goes to integer
+             auto fraction_shift = fraction_start + exponent;
+             if (fraction_shift > fraction_end)
+             {
+                 // The whole fraction and then some needs to go to the integer part
+                 garbage::read_single_number(fraction_start, fraction_end, &integer_part);
+
+                 auto power = fraction_shift - fraction_end;
+                 integer_part *= static_cast<NumericType>(std::pow(static_cast<NumericType>(10), power));
+             }
+             else
+             {
+                 // Only part of the fraction needs to go to the integer part
+                 garbage::read_single_number(fraction_start, fraction_shift, &integer_part);
+                 garbage::read_single_number(fraction_shift, fraction_end, &fractional_part);
+
+                 auto power = fraction_end - fraction_shift;
+                 fractional_divide = static_cast<NumericType>(std::pow(static_cast<NumericType>(10), power));
+             }
+         }
+         else
+         {
+             // The fractional divide will always be the same power, regardless of the integer length
+             auto power = (fraction_end - fraction_start) - exponent;
+             fractional_divide = static_cast<NumericType>(std::pow(static_cast<NumericType>(10), power));
+
+             // Some of the integer needs to go to the fraction
+             auto integer_shift = integer_end + exponent;
+             if (integer_start > integer_shift)
+             {
+                 // The whole integer and then some needs to go to the fractional part. We've already accounted for the
+                 // "and then some" above with the fractional_divide
+                 garbage::read_single_number(integer_start, integer_end, &fractional_part);
+             }
+             else
+             {
+                 // Only part of the integer needs to go to the fractional part
+                 garbage::read_single_number(integer_start, integer_shift, &integer_part);
+                 garbage::read_single_number(integer_shift, integer_end, &fractional_part);
+             }
+
+             garbage::read_single_number(fraction_start, fraction_end, &fractional_part);
+         }
+
+         auto result = integer_part + (fractional_part / fractional_divide);
+
+         // Negate if necessary; ignore if zero
+         if ((*string_front == '-') && ((integer_part != 0) || (fractional_part != 0)))
+         {
+             result = garbage::negate(result);
+         }
+
+         return result;
+     }
 
 #pragma endregion
 }
--- tests/NumericTests.cpp
+++ tests/NumericTests.cpp
@@ -19,7 +19,235 @@ namespace dhorn
     {
         TEST_CLASS(NumericTests)
         {
+#pragma region Helpers
 
+            static constexpr double tolerance = 1e-10;
+
+            template <
+                typename Ty,
+                typename CharT,
+                typename = typename std::enable_if<!std::is_same<Ty, double>::value>::type>
+            void DoTest(CharT *str, Ty expected)
+            {
+                auto val = numeric_cast<Ty>(str);
+                Assert::AreEqual(expected, val);
+            }
+
+            template <typename, typename CharT>
+            void DoTest(CharT *str, double expected)
+            {
+                auto val = numeric_cast<double>(str);
+                Assert::AreEqual(expected, val, tolerance);
+            }
+
+            template <typename Ty>
+            void ExpectException(char *str)
+            {
+                try
+                {
+                    numeric_cast<Ty>(str);
+                    Assert::Fail(L"Expected an exception");
+                }
+                catch (std::invalid_argument &)
+                {
+                }
+            }
+
+#pragma endregion
+
+
+
+#pragma region Signed Integer Tests
+
+            TEST_METHOD(SimpleSignedIntegerPositiveValueTest)
+            {
+                DoTest<int>(u8"0", 0);
+                DoTest<int>(u8"+1", 1);
+                DoTest<int>(u8"1337", 1337);
+            }
+
+            TEST_METHOD(SimpleSignedIntegerNegativeValueTest)
+            {
+                DoTest<int>(u8"-0", 0);
+                DoTest<int>(u8"-1", -1);
+                DoTest<int>(u8"-1337", -1337);
+            }
+
+            TEST_METHOD(SimpleSignedIntegerFractionalPositiveValueTest)
+            {
+                DoTest<int>(u8"0.5", 0);
+                DoTest<int>(u8".001", 0);
+                DoTest<int>(u8"+404.99", 404);
+            }
+
+            TEST_METHOD(SimpleSignedIntegerFractionalNegativeValueTest)
+            {
+                DoTest<int>(u8"-0.5", 0);
+                DoTest<int>(u8"-.001", 0);
+                DoTest<int>(u8"-99.99999999", -99);
+            }
+
+            TEST_METHOD(SimpleSignedIntegerExponentialPositiveValueTest)
+            {
+                DoTest<int>(u8"123.456e3", 123456);
+                DoTest<int>(u8"123.456e+1", 1234);
+                DoTest<int>(u8"123.456e0", 123);
+                DoTest<int>(u8"123.456E4", 1234560);
+                DoTest<int>(u8"123.456e-2", 1);
+                DoTest<int>(u8"123.456e-3", 0);
+                DoTest<int>(u8"123.456e-4", 0);
+                DoTest<int>(u8"1e2", 100);
+                DoTest<int>(u8"0e3", 0);
+                DoTest<int>(u8".123456e2", 12);
+            }
+
+            TEST_METHOD(SimpleSignedIntegerExponentialNegativeValueTest)
+            {
+                DoTest<int>(u8"-123.456e3", -123456);
+                DoTest<int>(u8"-123.456e+1", -1234);
+                DoTest<int>(u8"-123.456e0", -123);
+                DoTest<int>(u8"-123.456E4", -1234560);
+                DoTest<int>(u8"-123.456e-2", -1);
+                DoTest<int>(u8"-123.456e-3", 0);
+                DoTest<int>(u8"-123.456e-4", 0);
+                DoTest<int>(u8"-1e2", -100);
+                DoTest<int>(u8"-0e3", 0);
+                DoTest<int>(u8"-.123456e2", -12);
+            }
+
+            TEST_METHOD(LargeValueSignedIntegerPositiveValueTest)
+            {
+                // Max integer value is 2,147,483,647
+                DoTest<int>(u8"2147483647", 2147483647);
+                DoTest<int>(u8"0.2147483647e10", 2147483647);
+            }
+
+#pragma endregion
+
+
+
+#pragma region Unsigned Integer Tests
+
+            TEST_METHOD(SimpleUnsignedIntegerPositiveValueTest)
+            {
+                DoTest<unsigned int>(u8"0", 0u);
+                DoTest<unsigned int>(u8"+1", 1u);
+                DoTest<unsigned int>(u8"1337", 1337u);
+            }
+
+            TEST_METHOD(SimpleUnsignedIntegerNegativeValueTest)
+            {
+                // Negative zero is zero, so this should be okay
+                DoTest<unsigned int>(u8"-0", 0u);
+
+                ExpectException<unsigned int>(u8"-1");
+            }
+
+            TEST_METHOD(SimpleUnsignedIntegerFractionalPositiveValueTest)
+            {
+                DoTest<unsigned int>(u8"0.5", 0u);
+                DoTest<unsigned int>(u8".001", 0u);
+                DoTest<unsigned int>(u8"+404.99", 404u);
+            }
+
+            TEST_METHOD(SimpleUnsignedIntegerFractionalNegativeValueTest)
+            {
+                ExpectException<unsigned int>(u8"-0.5");
+            }
+
+            TEST_METHOD(SimpleUnsignedIntegerExponentialPositiveValueTest)
+            {
+                DoTest<unsigned int>(u8"123.456e3", 123456u);
+                DoTest<unsigned int>(u8"123.456e+1", 1234u);
+                DoTest<unsigned int>(u8"123.456e0", 123u);
+                DoTest<unsigned int>(u8"123.456E4", 1234560u);
+                DoTest<unsigned int>(u8"123.456e-2", 1u);
+                DoTest<unsigned int>(u8"123.456e-3", 0u);
+                DoTest<unsigned int>(u8"123.456e-4", 0u);
+                DoTest<unsigned int>(u8"1e2", 100u);
+                DoTest<unsigned int>(u8"0e3", 0u);
+                DoTest<unsigned int>(u8".123456e2", 12u);
+            }
+
+            TEST_METHOD(SimpleUnsignedIntegerExponentialNegativeValueTest)
+            {
+                // Negative zero is zero, so this should be okay
+                DoTest<unsigned int>(u8"-0.0e3", 0u);
+
+                ExpectException<unsigned int>(u8"-123.456e3");
+                ExpectException<unsigned int>(u8"-123.456e+1");
+                ExpectException<unsigned int>(u8"-123.456e0");
+                ExpectException<unsigned int>(u8"-123.456E4");
+                ExpectException<unsigned int>(u8"-123.456e-2");
+                ExpectException<unsigned int>(u8"-123.456e-3");
+                ExpectException<unsigned int>(u8"-123.456e-4");
+                ExpectException<unsigned int>(u8"-1e2");
+                ExpectException<unsigned int>(u8"-.123456e2");
+            }
+
+#pragma endregion
+
+
+
+#pragma region Double Tests
+
+            TEST_METHOD(SimpleDoublePositiveValueTest)
+            {
+                DoTest<double>(u8"0", 0.0);
+                DoTest<double>(u8"+1", 1.0);
+                DoTest<double>(u8"1337", 1337.0);
+            }
+
+            TEST_METHOD(SimpleDoubleNegativeValueTest)
+            {
+                DoTest<double>(u8"-0", 0.0);
+                DoTest<double>(u8"-1", -1.0);
+                DoTest<double>(u8"-1337", -1337.0);
+            }
+
+            TEST_METHOD(SimpleDoubleFractionalPositiveValueTest)
+            {
+                DoTest<double>(u8"0.5", 0.5);
+                DoTest<double>(u8".001", 0.001);
+                DoTest<double>(u8"+404.99", 404.99);
+            }
+
+            TEST_METHOD(SimpleDoubleFractionalNegativeValueTest)
+            {
+                DoTest<double>(u8"-0.5", -0.5);
+                DoTest<double>(u8"-.001", -0.001);
+                DoTest<double>(u8"-99.99999999", -99.99999999);
+            }
+
+            TEST_METHOD(SimpleDoubleExponentialPositiveValueTest)
+            {
+                DoTest<double>(u8"123.456e3", 123456.0);
+                DoTest<double>(u8"123.456e+1", 1234.56);
+                DoTest<double>(u8"123.456e0", 123.456);
+                DoTest<double>(u8"123.456E4", 1234560.0);
+                DoTest<double>(u8"123.456e-2", 1.23456);
+                DoTest<double>(u8"123.456e-3", 0.123456);
+                DoTest<double>(u8"123.456e-4", 0.0123456);
+                DoTest<double>(u8"1e2", 100.0);
+                DoTest<double>(u8"0e3", 0.0);
+                DoTest<double>(u8".123456e2", 12.3456);
+            }
+
+            TEST_METHOD(SimpleDoubleExponentialNegativeValueTest)
+            {
+                DoTest<double>(u8"-123.456e3", -123456.0);
+                DoTest<double>(u8"-123.456e+1", -1234.56);
+                DoTest<double>(u8"-123.456e0", -123.456);
+                DoTest<double>(u8"-123.456E4", -1234560.0);
+                DoTest<double>(u8"-123.456e-2", -1.23456);
+                DoTest<double>(u8"-123.456e-3", -0.123456);
+                DoTest<double>(u8"-123.456e-4", -0.0123456);
+                DoTest<double>(u8"-1e2", -100.0);
+                DoTest<double>(u8"-0e3", 0.0);
+                DoTest<double>(u8"-.123456e2", -12.3456);
+            }
+
+#pragma endregion
         };
     }
 }
