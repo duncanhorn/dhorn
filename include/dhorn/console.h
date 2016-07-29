6bfe6a9c8fa59e5d1347d30a8bf090d679df2029
Duncan Horn <duncan.horn@utexas.edu>
2016-07-28 21:03:09 -0700

    removing console_color_intensity; it is now a part of console_color

--- include/dhorn/console.h
+++ include/dhorn/console.h
@@ -36,28 +36,29 @@ namespace dhorn
      */
     enum class console_color : uint8_t
     {
-        red     = 0x04,
-        green   = 0x02,
-        blue    = 0x01,
-        magenta = (red | blue),
-        cyan    = (green | blue),
-        yellow  = (red | green),
+        black        = 0x00,
+        dark_gray    = FOREGROUND_INTENSITY,
 
-        white   = (red | green | blue),
-        black   = 0x00,
-    };
+        dark_red     = FOREGROUND_RED,
+        red          = (dark_red | dark_gray),
 
+        dark_green   = FOREGROUND_GREEN,
+        green        = (dark_green | dark_gray),
 
+        dark_blue    = FOREGROUND_BLUE,
+        blue         = (dark_blue | dark_gray),
 
-    /*
-     * console_color_intensity
-     *
-     * Represents the intensity of a foreground or background color used within the console
-     */
-    enum class console_color_intensity : uint8_t
-    {
-        not_intense = 0x00,
-        intense     = 0x08,
+        dark_magenta = (dark_red | dark_blue),
+        magenta      = (dark_magenta | dark_gray),
+
+        dark_cyan    = (dark_green | dark_blue),
+        cyan         = (dark_cyan | dark_gray),
+
+        dark_yellow  = (dark_red | dark_green),
+        yellow       = (dark_yellow | dark_gray),
+
+        gray         = (dark_red | dark_green | dark_blue),
+        white        = (dark_gray | dark_gray),
     };
 
 
@@ -92,40 +93,28 @@ namespace dhorn
      */
 #pragma region Console Size
 
-    inline size<int16_t> console_size(_In_ console_device device)
+    inline size<int16_t> console_size(_In_ console_device device = console_device::output)
     {
         auto info = details::console_info(device);
         return { info.dwSize.X, info.dwSize.Y };
     }
 
-    inline point<int16_t> console_cursor_position(_In_ console_device device)
+    inline point<int16_t> console_cursor_position(_In_ console_device device = console_device::output)
     {
         auto info = details::console_info(device);
         return { info.dwCursorPosition.X, info.dwCursorPosition.Y };
     }
 
-    inline console_color console_foreground_color(_In_ console_device device)
+    inline console_color console_foreground_color(_In_ console_device device = console_device::output)
     {
         auto info = details::console_info(device);
-        return static_cast<console_color>(info.wAttributes & 0x07);
+        return static_cast<console_color>(info.wAttributes & 0x0F);
     }
 
-    inline console_color console_foreground_intensity(_In_ console_device device)
+    inline console_color console_background_color(_In_ console_device device = console_device::output)
     {
         auto info = details::console_info(device);
-        return static_cast<console_color>(info.wAttributes & 0x08);
-    }
-
-    inline console_color console_background_color(_In_ console_device device)
-    {
-        auto info = details::console_info(device);
-        return static_cast<console_color>((info.wAttributes >> 4) & 0x07);
-    }
-
-    inline console_color console_background_intensity(_In_ console_device device)
-    {
-        auto info = details::console_info(device);
-        return static_cast<console_color>((info.wAttributes >> 4) & 0x08);
+        return static_cast<console_color>((info.wAttributes >> 4) & 0x0F);
     }
 
 #pragma endregion
@@ -138,9 +127,8 @@ namespace dhorn
 #pragma region Console Text Modifications
 
     inline auto change_console_foreground(
-        _In_ console_device device,
         _In_ console_color color,
-        _In_ console_color_intensity intensity = console_color_intensity::intense)
+        _In_ console_device device = console_device::output)
     {
         auto handle = ::GetStdHandle(static_cast<DWORD>(device));
         auto info = details::console_info(handle);
@@ -149,9 +137,8 @@ namespace dhorn
             ::SetConsoleTextAttribute(handle, attr);
         });
 
-        auto flags = static_cast<uint8_t>(color) | static_cast<uint8_t>(intensity);
         info.wAttributes = clear_flag(info.wAttributes, 0x0F);
-        info.wAttributes = set_flag(info.wAttributes, flags);
+        info.wAttributes = set_flag(info.wAttributes, static_cast<uint8_t>(color));
         if (!::SetConsoleTextAttribute(handle, info.wAttributes))
         {
             result.cancel();
@@ -162,9 +149,8 @@ namespace dhorn
     }
 
     inline auto change_console_background(
-        _In_ console_device device,
         _In_ console_color color,
-        _In_ console_color_intensity intensity = console_color_intensity::intense)
+        _In_ console_device device = console_device::output)
     {
         auto handle = ::GetStdHandle(static_cast<DWORD>(device));
         auto info = details::console_info(handle);
@@ -173,9 +159,8 @@ namespace dhorn
             ::SetConsoleTextAttribute(handle, attr);
         });
 
-        auto flags = static_cast<uint8_t>(color) | static_cast<uint8_t>(intensity);
         info.wAttributes = clear_flag(info.wAttributes, 0x00F0);
-        info.wAttributes = set_flag(info.wAttributes, flags << 4);
+        info.wAttributes = set_flag(info.wAttributes, static_cast<uint8_t>(color) << 4);
         if (!::SetConsoleTextAttribute(handle, info.wAttributes))
         {
             result.cancel();
@@ -186,11 +171,9 @@ namespace dhorn
     }
 
     inline auto change_console_colors(
-        _In_ console_device device,
         _In_ console_color foregroundColor,
         _In_ console_color backgroundColor,
-        _In_ console_color_intensity foregroundIntensity = console_color_intensity::intense,
-        _In_ console_color_intensity backgroundIntensity = console_color_intensity::intense)
+        _In_ console_device device = console_device::output)
     {
         auto handle = ::GetStdHandle(static_cast<DWORD>(device));
         auto info = details::console_info(handle);
@@ -199,12 +182,9 @@ namespace dhorn
             ::SetConsoleTextAttribute(handle, attr);
         });
 
-        auto foregroundFlags = static_cast<uint8_t>(foregroundColor) | static_cast<uint8_t>(foregroundIntensity);
-        auto backgroundFlags = static_cast<uint8_t>(backgroundColor) | static_cast<uint8_t>(backgroundIntensity);
-
         info.wAttributes = clear_flag(info.wAttributes, 0x00FF);
-        info.wAttributes = set_flag(info.wAttributes, foregroundFlags);
-        info.wAttributes = set_flag(info.wAttributes, backgroundFlags << 4);
+        info.wAttributes = set_flag(info.wAttributes, static_cast<uint8_t>(foregroundColor));
+        info.wAttributes = set_flag(info.wAttributes, static_cast<uint8_t>(backgroundColor) << 4);
         if (!::SetConsoleTextAttribute(handle, info.wAttributes))
         {
             result.cancel();
