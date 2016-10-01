dcdc3d82202aa03d969c954082cdea619989460d
Duncan Horn <duncan.horn@utexas.edu>
2016-10-01 01:11:24 -0700

    Moving bitmask out of experimental namesapce

--- tests/BitmaskTests.cpp
+++ tests/BitmaskTests.cpp
@@ -31,78 +31,78 @@ namespace dhorn
             TEST_METHOD(SetFlagTest)
             {
                 int value = 0;
-                value = dhorn::experimental::set_flag(value, 0x01);
+                value = dhorn::set_flag(value, 0x01);
                 Assert::AreEqual(0x01, value);
 
                 // Setting twice shouldn't do anything
-                value = dhorn::experimental::set_flag(value, 0x01);
+                value = dhorn::set_flag(value, 0x01);
                 Assert::AreEqual(0x01, value);
             }
 
             TEST_METHOD(ClearFlagTest)
             {
                 int value = 0x03;
-                value = dhorn::experimental::clear_flag(value, 0x01);
+                value = dhorn::clear_flag(value, 0x01);
                 Assert::AreEqual(0x02, value);
 
                 // Clearing twice shouldn't do anything
-                value = dhorn::experimental::clear_flag(value, 0x01);
+                value = dhorn::clear_flag(value, 0x01);
                 Assert::AreEqual(0x02, value);
 
                 // Clearing a non-set flag should be okay
-                value = dhorn::experimental::clear_flag(value, 0x04);
+                value = dhorn::clear_flag(value, 0x04);
                 Assert::AreEqual(0x02, value);
 
-                value = dhorn::experimental::clear_flag(value, 0x0F);
+                value = dhorn::clear_flag(value, 0x0F);
                 Assert::AreEqual(0, value);
             }
 
             TEST_METHOD(ToggleFlagTest)
             {
                 int value = 0;
-                value = dhorn::experimental::toggle_flag(value, 0x05);
+                value = dhorn::toggle_flag(value, 0x05);
                 Assert::AreEqual(0x05, value);
-                value = dhorn::experimental::toggle_flag(value, 0x0A);
+                value = dhorn::toggle_flag(value, 0x0A);
                 Assert::AreEqual(0x0F, value);
 
-                value = dhorn::experimental::toggle_flag(value, 0x05);
+                value = dhorn::toggle_flag(value, 0x05);
                 Assert::AreEqual(0x0A, value);
-                value = dhorn::experimental::toggle_flag(value, 0x0A);
+                value = dhorn::toggle_flag(value, 0x0A);
                 Assert::AreEqual(0x00, value);
             }
 
             TEST_METHOD(IsAnyFlagSetTest)
             {
                 int value = 0x03;
-                Assert::IsTrue(dhorn::experimental::is_any_flag_set(value, 0x01));
-                Assert::IsTrue(dhorn::experimental::is_any_flag_set(value, 0x02));
-                Assert::IsTrue(dhorn::experimental::is_any_flag_set(value, 0x03));
-                Assert::IsFalse(dhorn::experimental::is_any_flag_set(value, 0x04));
-                Assert::IsFalse(dhorn::experimental::is_any_flag_set(value, 0x00));
-                Assert::IsTrue(dhorn::experimental::is_any_flag_set(value, 0xFF));
+                Assert::IsTrue(dhorn::is_any_flag_set(value, 0x01));
+                Assert::IsTrue(dhorn::is_any_flag_set(value, 0x02));
+                Assert::IsTrue(dhorn::is_any_flag_set(value, 0x03));
+                Assert::IsFalse(dhorn::is_any_flag_set(value, 0x04));
+                Assert::IsFalse(dhorn::is_any_flag_set(value, 0x00));
+                Assert::IsTrue(dhorn::is_any_flag_set(value, 0xFF));
             }
 
             TEST_METHOD(AreAllFlagsSetTest)
             {
                 int value = 0x03;
-                Assert::IsTrue(dhorn::experimental::are_all_flags_set(value, 0x01));
-                Assert::IsTrue(dhorn::experimental::are_all_flags_set(value, 0x02));
-                Assert::IsTrue(dhorn::experimental::are_all_flags_set(value, 0x03));
-                Assert::IsTrue(dhorn::experimental::are_all_flags_set(value, 0x00));
-                Assert::IsFalse(dhorn::experimental::are_all_flags_set(value, 0x04));
-                Assert::IsFalse(dhorn::experimental::are_all_flags_set(value, 0x07));
-                Assert::IsFalse(dhorn::experimental::are_all_flags_set(value, 0xFF));
+                Assert::IsTrue(dhorn::are_all_flags_set(value, 0x01));
+                Assert::IsTrue(dhorn::are_all_flags_set(value, 0x02));
+                Assert::IsTrue(dhorn::are_all_flags_set(value, 0x03));
+                Assert::IsTrue(dhorn::are_all_flags_set(value, 0x00));
+                Assert::IsFalse(dhorn::are_all_flags_set(value, 0x04));
+                Assert::IsFalse(dhorn::are_all_flags_set(value, 0x07));
+                Assert::IsFalse(dhorn::are_all_flags_set(value, 0xFF));
             }
 
             TEST_METHOD(AreAllFlagsClearTest)
             {
                 int value = 0x03;
-                Assert::IsFalse(dhorn::experimental::are_all_flags_clear(value, 0x03));
-                Assert::IsFalse(dhorn::experimental::are_all_flags_clear(value, 0x01));
-                Assert::IsFalse(dhorn::experimental::are_all_flags_clear(value, 0x02));
-                Assert::IsTrue(dhorn::experimental::are_all_flags_clear(value, 0x00));
-                Assert::IsTrue(dhorn::experimental::are_all_flags_clear(value, 0xFC));
-                Assert::IsTrue(dhorn::experimental::are_all_flags_clear(value, 0xF0));
+                Assert::IsFalse(dhorn::are_all_flags_clear(value, 0x03));
+                Assert::IsFalse(dhorn::are_all_flags_clear(value, 0x01));
+                Assert::IsFalse(dhorn::are_all_flags_clear(value, 0x02));
+                Assert::IsTrue(dhorn::are_all_flags_clear(value, 0x00));
+                Assert::IsTrue(dhorn::are_all_flags_clear(value, 0xFC));
+                Assert::IsTrue(dhorn::are_all_flags_clear(value, 0xF0));
             }
 
             TEST_METHOD(EnumOperatorOrTest)
@@ -147,7 +147,7 @@ namespace dhorn
                 static_assert(~test_enum::value0 == test_enum::invalid, "Should be static_assert-able");
 
                 // clear_flags uses operator~
-                Assert::IsTrue(dhorn::experimental::clear_flags(test_enum::value3, test_enum::value2) ==
+                Assert::IsTrue(dhorn::clear_flags(test_enum::value3, test_enum::value2) ==
                     test_enum::value1);
             }
         };
