f084d0f5002072e32782e5fdd5e6efdd86e73efe
Duncan Horn <duncan.horn@utexas.edu>
2016-10-03 00:38:22 -0700

    Updating comments

--- include/dhorn/experimental/scope_guard.h
+++ include/dhorn/experimental/scope_guard.h
@@ -3,13 +3,42 @@
  *
  * scope_guard.h
  *
- * 
-
-
- * Simple way to declare an object whose destructor functionality you can define. E.g. if you are needing to set some
- * variable at the end of a function regardless of whether or not there was an exception, then you could do:
+ * An RAII wrapper for invoking a specified function object at the destruction of the scope_guard object, or manually
+ * via its `operator()` function. A scope_guard object will only invoke the function object once, and will no-op if it
+ * has previously been invoked, or if it has been manually cancelled. For example:
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * void doit(int &value)
+ * {
+ *     auto incrementValue = make_scope_guard([&]() { ++value; });
+ * }
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * If the above function is invoked as-is, when `doit` returns, `value` will be incremented. If the following code is
+ * added to the example above:
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *     incrementValue();
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Then the result will still be the same (value is incremented once). Note the phrasing "incremented once." That is,
+ * `value` does not get incremented again as a result of the scope_guard getting destroyed. E.g. the following example
+ * results in the same (`value` is incremented once):
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *     incrementValue();
+ *     incrementValue();
+ *     incrementValue();
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * However, if the original example was changed to:
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *     incrementValue.cancel();
+ *     incrementValue();
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Then `value` is *not* incremented at all. Note, however, that if the order is reversed:
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *     incrementValue();
+ *     incrementValue.cancel();
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Then `value` is still incremented once since the scope_guard was invoked before it was cancelled.
  *
- *  dhorn::scope_guard cleanup([&]() { this->_updating = false; });
+ * Note that scope_guard's destructor is noexcept (just like any other destructor by default) and therefore
+ * std::terminate will get called if the function object throws an exception that it does not handle.
  */
 #pragma once
 
@@ -89,6 +118,13 @@ namespace dhorn
             bool _shouldInvoke = true;
         };
 
+
+
+        /*
+         * make_scope_guard
+         *
+         * Makes a scope_guard object for the input function object (generally a lambda).
+         */
         template <typename FuncTy>
         inline scope_guard<std::decay_t<FuncTy>> make_scope_guard(FuncTy&& func)
         {
