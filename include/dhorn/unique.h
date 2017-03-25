56a0ceebd128eaab460791b9e916f0442b42c685
Duncan Horn <duncan.horn@utexas.edu>
2017-03-24 23:54:45 -0700

    Think that the design is coming along...

--- include/dhorn/unique.h
+++ include/dhorn/unique.h
@@ -1,7 +1,30 @@
 /*
  * Duncan Horn
  *
- * unique_any.h
+ * unique.h
+ *
+ * The `dhorn::unique` type is a generic resource management type modeled after `std::unique_ptr`, but with the
+ * difference that it can manage _any_ type, not just pointers. The `unique` type takes two template arguments:
+ *
+ *      `Ty`            Represents the "effective type" or "semantic type" of the `unique` type. Objects are said to
+ *                      "act like" this type. For example, a template argument of `int*` means that the object "acts
+ *                      like" a pointer to a single integer. Similarly, a type of `int(*)[]` "acts like" a pointer to an
+ *                      array of integers. Unlike `std::unique_ptr`, a type of `T[]` for some `T` does _not_ "act like"
+ *                      a pointer to an array of `T`s.
+ *      `Traits`        Describes how the underlying resource type is managed. The full functionality of this type is
+ *                      decribed later, but in general this type described how the type is stored, how the resource's
+ *                      lifetime is managed, and properties of this type such as validity and default construction.
+ *
+ * Each `unique` object provides several type aliases that are summarized as follows
+ *
+ *      value_type      The type that is stored internally to the `unique` object. Operations that construct or modify
+ *                      a `unique` object accept `value_type` (or an l/r-value reference to `value_type`) as the
+ *                      argument type. Additionally, accessor functions return objects of `value_type` (or l/r-value
+ *                      references to `value_type`). This is an alias for `Traits::value_type` if it exists. Otherwise
+ *                      it is an alias for `Ty`.
+ *      semantic_type   An alias for `Ty`. This type is primarily used for overload resolution purposes for construction
+ *                      or assignment to the underlying resource member variable.
+ *      traits_type     An alias for `Traits`.
  *
  * 
  */
@@ -10,238 +33,304 @@
 #include <memory>
 #include <tuple>
 
+#include "compressed_pair.h"
+
 namespace dhorn
 {
     /*
-     * unique_any Traits Types
-     *
-     * Expected to provide the following type aliases:
-     *      * value_type    This is the type returned by `unique_any` to its caller. In general, this should be the same
-     *                      as the template type, but there are some obvious exceptions (e.g. arrays).
-     *      * storage_type  This is the type that `unique_any` stores internally and is the type that gets passed to the
-     *                      static functions on the traits type. This allows things like custom deleters, etc. without
-     *                      dirtying the user-facing API.
-     *
-     * Expected to provide the following static member functions:
-     *      * default_value Returns a `storage_type` object that represents the value of a default constructed
-     *                      `unique_any` object. Note that unlike the `construct` function, the return value of this
-     *                      function is _not_ a `std::tuple` since it is used for `reset`, etc. as well.
-     *      * construct     Returns a `std::tuple` that is used to construct the `storage_type` member. In general, this
-     *                      should return `std::forward_as_tuple` for the input argument(s). Note that this function
-     *                      function is _not_ called for the default constructor (`default_value` is).
-     *      * destroy       
-     *      * valid         Tests a `storage_type` value to see if it contains a valid value (e.g. non-null, etc.).
+     * unique Traits Types
      */
-#pragma region unique_any Traits Types
+#pragma region unique Traits Types
 
     /*
      * unique_pointer_traits
      */
     template <typename Ty, typename Deleter = std::default_delete<Ty>>
-    struct unique_pointer_traits
+    struct unique_pointer_traits :
+        private details::compressed_base<Deleter>
     {
-        using value_type = Ty*;
-    };
-
-
-
-    /*
-     * unique_any_traits
-     */
-    template <typename Ty>
-    struct unique_any_traits;
-
-    template <typename Ty>
-    struct unique_any_traits<Ty*>
-    {
-        using value_type = Ty*;
-
-        static constexpr bool valid(value_type ptr) noexcept
-        {
-            return (ptr != nullptr);
-        }
-
-        static constexpr value_type default_value() noexcept
-        {
-            return nullptr;
-        }
-
-        static void destroy(value_type ptr) noexcept
-        {
-            delete ptr;
-        }
-    };
-
-    template <typename Ty>
-    struct unique_any_traits<Ty[]> :
-        public unique_any_traits<Ty*>
-    {
-        static void destroy(Ty* ptr) noexcept
-        {
-            delete[] ptr;
-        }
-    };
-
-#pragma endregion
-
-
-
-    /*
-     * unique_any
-     */
-    template <typename Ty, typename Traits = unique_any_traits<Ty>>
-    class unique_any
-    {
-        // Validate properties on the traits type
-        static_assert(std::is_constructible_v<Ty, decltype(Traits::default_value())>,
-            "Must be able to construct value_type with the result of default_value");
-        static_assert(std::is_assignable_v<Ty&, decltype(Traits::default_value())>,
-            "Must be able to assign the result of default_value to value_type");
+    private:
+        using deleter_base = details::compressed_base<Deleter>;
 
     public:
         /*
-         * Public Types
+         * Type Aliases
          */
-        using value_type = typename Traits::value_type;
+        using value_type = Ty*;
 
 
 
         /*
          * Constructor(s)/Destructor
          */
-#pragma region Constructor(s)/Destructor
-
-        // Default/no-arg Construction
-        template <std::enable_if_t<std::is_convertible_v<decltype(Traits::default_value()), value_type>, int> = 0>
-        constexpr unique_any() noexcept(std::is_nothrow_constructible_v<value_type, decltype(Traits::default_value())>) :
-            _value(Traits::default_value())
-        {
-        }
-
-        template <std::enable_if_t<!std::is_convertible_v<decltype(Traits::default_value()), value_type>, int> = 0>
-        explicit constexpr unique_any() noexcept(std::is_nothrow_constructible_v<value_type, decltype(Traits::default_value())>) :
-            _value(Traits::default_value())
-        {
-        }
-
-        // Value Construction
-        //template <
-        //    std::enable_if_t<std::is_copy_constructible_v<value_type>, int> = 0,
-        //    std::enable_if_t<std::is_convertible_v<const value_type&, value_type>, int> = 0>
-        //constexpr unique_any(const value_type& value) noexcept(std::is_nothrow_copy_constructible_v<value_type>) :
-        //    _value(value)
-        //{
-        //}
-        //
-        //template <
-        //    std::enable_if_t<std::is_copy_constructible_v<value_type>, int> = 0,
-        //    std::enable_if_t<!std::is_convertible_v<const value_type&, value_type>, int> = 0>
-        //explicit constexpr unique_any(const value_type& value) noexcept(std::is_nothrow_copy_constructible_v<value_type>) :
-        //    _value(value)
-        //{
-        //}
-
-        // R-Value Construction
-        template <
-            typename OtherTy,
-            std::enable_if_t<std::is_constructible_v<value_type, OtherTy&&>, int> = 0,
-            std::enable_if_t<std::is_convertible_v<OtherTy&&, value_type>, int> = 0>
-        constexpr unique_any(OtherTy&& value) noexcept(std::is_nothrow_constructible_v<value_type, OtherTy&&>) :
-            _value(std::forward<OtherTy>(value))
-        {
-        }
-
-        template <
-            typename OtherTy,
-            std::enable_if_t<std::is_constructible_v<value_type, OtherTy&&>, int> = 0,
-            std::enable_if_t<!std::is_convertible_v<OtherTy&&, value_type>, int> = 0>
-        explicit constexpr unique_any(OtherTy&& value) noexcept(std::is_nothrow_constructible_v<value_type, OtherTy&&>) :
-            _value(std::forward<OtherTy>(value))
-        {
-        }
-
-        // Cannot copy unique objects
-        unique_any(const unique_any&) = delete;
-
-        // Move Construction
-        template <
-            typename OtherTy,
-            std::enable_if_t<std::is_constructible_v<value_type, OtherTy&&>, int> = 0,
-            std::enable_if_t<std::is_convertible_v<OtherTy&&, value_type>, int> = 0>
-        unique_any(unique_any<OtherTy>&& other) noexcept(std::is_nothrow_constructible_v<value_type, OtherTy&&>) :
-            _value(std::move(other._value))
-        {
-            // TODO: Detach/invalidate other
-        }
-        
-        template <
-            typename OtherTy,
-            std::enable_if_t<std::is_constructible_v<value_type, OtherTy&&>, int> = 0,
-            std::enable_if_t<!std::is_convertible_v<OtherTy&&, value_type>, int> = 0>
-        explicit unique_any(unique_any<OtherTy>&& other) noexcept(std::is_nothrow_constructible_v<value_type, OtherTy&&>) :
-            _value(std::move(other._value))
-        {
-            // TODO: Detach/invalidate other
-        }
-
-        ~unique_any()
-        {
-            // TODO: reset();
-        }
-
-#pragma endregion
+        using deleter_base::deleter_base;
 
 
 
         /*
-         * Operators
+         * Destroy Operator
          */
-#pragma region Operators
-
-        unique_any& operator=(const unique_any&) = delete;
-
-        template <
-            typename OtherTy,
-            std::enable_if_t<std::is_assignable_v<value_type&, OtherTy&&>, int> = 0>
-        unique_any& operator=(unique_any<OtherTy>&& other) noexcept(std::is_nothrow_assignable_v<value_type, OtherTy&&>)
+        void operator()(value_type ptr)
         {
-
+            deleter_base::value()(ptr);
         }
-
-        //unique_any& operator=(unique_any&& other) noexcept(swap(other))
-        //{
-        //    other.swap(*this);
-        //    return *this;
-        //}
-
-        operator bool() const noexcept
-        {
-            return Traits::valid(this->_value);
-        }
-
-#pragma endregion
 
 
 
         /*
-         * 
+         * Static Traits Functions
          */
-#pragma region
-
-        void swap(unique_any& other) noexcept(
-            std::is_nothrow_move_constructible_v<value_type> &&
-            std::is_nothrow_swappable_v<value_type>)
+        static constexpr bool is_valid(value_type ptr) noexcept
         {
-            using std::swap;
-            swap(this->_value, other._value);
+            return ptr != nullptr;
         }
 
+        static constexpr value_type default_value() noexcept
+        {
+            return nullptr;
+        }
+    };
+
+
+
+    /*
+     * unique_array_traits
+     */
+    template <typename Ty, typename Deleter = std::default_delete<Ty[]>>
+    struct unique_array_traits :
+        private details::compressed_base<Deleter>
+    {
+    private:
+        using deleter_base = details::compressed_base<Deleter>;
+
+    public:
+        /*
+         * Type Aliases
+         */
+        using value_type = Ty*;
+
+
+
+        /*
+         * Constructor(s)/Destructor
+         */
+        using deleter_base::deleter_base;
+
+
+
+        /*
+         * Destroy Operator
+         */
+        void operator()(value_type ptr)
+        {
+            deleter_base::value()(ptr);
+        }
+
+
+
+        /*
+         * Static Traits Functions
+         */
+        static constexpr bool is_valid(value_type ptr) noexcept
+        {
+            return ptr != nullptr;
+        }
+
+        static constexpr value_type default_value() noexcept
+        {
+            return nullptr;
+        }
+    };
+
+
+
+    /*
+     * unique_traits
+     */
+    template <typename Ty>
+    struct unique_traits;
+
+    template <typename Ty>
+    struct unique_traits<Ty*> :
+        public unique_pointer_traits<Ty>
+    {
+    };
+
+    template <typename Ty>
+    struct unique_traits<Ty(*)[]> :
+        public unique_array_traits<Ty>
+    {
+    };
+
 #pragma endregion
+
+
+
+    /*
+     * unique
+     */
+#pragma region unique
+
+    /*
+     * Helpers
+     */
+    namespace details
+    {
+        /*
+         * value_type_from_unique_traits
+         */
+        template <typename TraitsTy, typename Ty, typename = void>
+        struct value_type_from_unique_traits
+        {
+            // Fallback case
+            using type = Ty;
+        };
+
+        template <typename TraitsTy, typename Ty>
+        struct value_type_from_unique_traits<TraitsTy, Ty, std::void_t<typename TraitsTy::value_type>>
+        {
+            using type = typename TraitsTy::value_type;
+        };
+
+        template <typename TraitsTy, typename Ty>
+        using value_type_from_unique_traits_t = typename value_type_from_unique_traits<TraitsTy, Ty>::type;
+    }
+
+
+
+    template <typename Ty, typename Traits = unique_traits<Ty>>
+    class unique
+    {
+    public:
+        /*
+         * Public Types
+         */
+        using traits_type = Traits;
+        using value_type = details::value_type_from_unique_traits_t<Traits, Ty>;
+        using semantic_type = Ty;
+
+
+
+        /*
+         * Constructor(s)/Destructor
+         */
+        constexpr unique() noexcept :
+            _data()
+        {
+        }
+
+        template <typename Type, std::enable_if_t<std::is_constructible<Ty, const Type&>::value, int> = 0>
+        explicit unique(const Type& value) :
+            _data(value, traits_type{})
+        {
+            static_assert(!std::is_pointer_v<Traits>,
+                "Construcing a unique object with a null traits object is ill-formed");
+        }
+
+        //explicit unique(const value_type& value) :
+        //    _data(value, traits_type{})
+        //{
+        //    static_assert(!std::is_pointer_v<Traits>,
+        //        "Construcing a unique object with a null traits object is ill-formed");
+        //}
+
+        ~unique()
+        {
+            reset();
+        }
+
+
+
+        /*
+         * Modifiers
+         */
+        value_type release() noexcept // <-- TOOD: Should this be conditional?
+        {
+            value_type result = traits_type::default_value();
+            std::swap(result, my_value());
+            return result;
+        }
+
+        void reset() noexcept // <-- TODO: Should this be conditional
+        {
+            value_type value = traits_type::default_value();
+            std::swap(value, my_value());
+            my_traits()(value);
+        }
+
+        template <typename Type, std::enable_if_t<std::is_convertible<const Type&, value_type>::value, int> = 0>
+        void reset(const Type& value) noexcept // <-- TODO: Should this be conditional
+        {
+            my_traits()(my_value());
+            my_value() = value;
+        }
+
+        void reset(value_type&& value) noexcept // <-- TODO: Should this be conditional
+        {
+            my_traits()(my_value());
+            my_value() = std::move(value);
+        }
+
+
+
+        /*
+         * Observers
+         */
 
 
 
     private:
 
-        value_type _value;
+        constexpr value_type& my_value() noexcept
+        {
+            return this->_data.first();
+        }
+
+        constexpr traits_type& my_traits() noexcept
+        {
+            return this->_data.second();
+        }
+
+        compressed_pair<value_type, traits_type> _data;
     };
+
+#pragma endregion
+
+
+
+    /*
+     * unique Specializations
+     */
+#pragma region unique Specializations
+
+    /*
+     * Helpers
+     */
+    namespace details
+    {
+        template <typename Ty>
+        struct select_pointer_for_unique
+        {
+            using type = Ty*;
+        };
+
+        template <typename Ty>
+        struct select_pointer_for_unique<Ty[]>
+        {
+            using type = Ty(*)[];
+        };
+
+        template <typename Ty>
+        using select_pointer_for_unique_t = typename select_pointer_for_unique<Ty>::type;
+    }
+
+
+
+    /*
+     * unique_ptr
+     */
+    template <typename Ty>
+    using unique_ptr = unique<details::select_pointer_for_unique_t<Ty>>;
+
+#pragma endregion
 }
--- tests/UniqueTests.cpp
+++ tests/UniqueTests.cpp
@@ -15,6 +15,47 @@ namespace dhorn::tests
 {
     TEST_CLASS(UniqueTests)
     {
+#pragma region Test Traits
+
+        // Traits type that is guaranteed to be empty. Useful for size tests...
+        template <typename Ty>
+        struct empty_traits
+        {
+            // Decay arrays to pointers
+            using value_type = std::decay_t<Ty>;
+
+            static constexpr bool is_valid(const value_type&) { return true; }
+            static constexpr value_type default_value() { return value_type{}; }
+
+            constexpr void operator()(const value_type&) {}
+        };
+
+        template <typename Ty>
+        using unique_empty = unique<Ty, empty_traits<Ty>>;
+
+
+
+        template <typename Ty>
+        struct non_empty_traits
+        {
+            // Decay arrays to pointers
+            using value_type = std::decay_t<Ty>;
+
+            static constexpr bool is_valid(const value_type&) { return true; }
+            static constexpr value_type default_value() { return value_type{}; }
+
+            constexpr void operator()(const value_type&) {}
+
+            // Hold a `Ty` as the member. This makes our math easier (multiply by two) and eliminates our need to worry
+            // about padding
+            value_type value;
+        };
+
+        template <typename Ty>
+        using unique_non_empty = unique<Ty, non_empty_traits<Ty>>;
+
+
+
         template <typename CharT>
         struct unique_basic_string_traits
         {
@@ -35,7 +76,88 @@ namespace dhorn::tests
                 // Strings delete themselves
             }
         };
-        using unique_string = unique_any<std::string, unique_basic_string_traits<char>>;
-        using unique_wstring = unique_any<std::wstring, unique_basic_string_traits<wchar_t>>;
+
+        template <typename CharT>
+        using unique_basic_string = unique < std::basic_string<CharT>, unique_basic_string_traits<CharT>>;
+
+
+
+        template <typename Ty, size_t Size, Ty Value = Ty{}>
+        struct clear_buffer_unique_traits
+        {
+            using value_type = Ty*;
+
+            static constexpr bool valid(value_type)
+            {
+                // Should never be given a null value, so always valid
+                return true;
+            }
+
+            [[noreturn]] static constexpr value_type default_value()
+            {
+                Assert::Fail("Did not expect default construction");
+            }
+
+            static void destroy(value_type value)
+            {
+                for (size_t i = 0; i < Size; ++i)
+                {
+                    value[i] = Value;
+                }
+            }
+        };
+
+        template <typename Ty, size_t Size, Ty Value = Ty{}>
+        using unique_buffer = unique<Ty, clear_buffer_unique_traits<Ty, Size, Value>>;
+
+#pragma endregion
+
+
+
+#pragma region Size Tests
+
+        TEST_METHOD(EmptyTraitsSizeTest)
+        {
+            Assert::AreEqual(sizeof(int), sizeof(unique_empty<int>));
+            Assert::AreEqual(sizeof(std::string), sizeof(unique_empty<std::string>));
+
+            Assert::AreEqual(sizeof(int*), sizeof(unique_empty<int*>));
+            Assert::AreEqual(sizeof(int*), sizeof(unique_empty<int[]>));
+        }
+
+        TEST_METHOD(NonEmptyTraitsSizeTest)
+        {
+            Assert::AreEqual(sizeof(int) * 2, sizeof(unique_non_empty<int>));
+            Assert::AreEqual(sizeof(std::string) * 2, sizeof(unique_non_empty<std::string>));
+
+            Assert::AreEqual(sizeof(int*) * 2, sizeof(unique_non_empty<int*>));
+            Assert::AreEqual(sizeof(int*) * 2, sizeof(unique_non_empty<int[]>));
+        }
+
+        TEST_METHOD(UniquePtrSizeTest)
+        {
+            Assert::AreEqual(sizeof(void*), sizeof(unique_ptr<int>));
+            Assert::AreEqual(sizeof(void*), sizeof(unique_ptr<std::string>));
+
+            Assert::AreEqual(sizeof(void*), sizeof(unique_ptr<int[]>));
+        }
+
+#pragma endregion
+
+
+
+#pragma region Tests
+
+        TEST_METHOD(DefaultConstructionTest)
+        {
+            unique_ptr<int> intPtr;
+        }
+
+        TEST_METHOD(ClearBufferTest)
+        {
+
+        }
+
+#pragma endregion
     };
 }
