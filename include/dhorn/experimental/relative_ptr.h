d98d8e037e4d11e6006b580cfa56a886b9ff67a3
Duncan Horn <duncan.horn@utexas.edu>
2017-08-26 23:51:44 -0700

    Most of the way there

--- include/dhorn/experimental/relative_ptr.h
+++ include/dhorn/experimental/relative_ptr.h
@@ -7,46 +7,225 @@
  */
 #pragma once
 
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
-
-// TODO: Make a decision about alignment
+#include <iterator>
 
 namespace dhorn::experimental
 {
+    /*
+     * relative_ptr
+     *
+     * TODO: Make a decision about alignment
+     * TODO: How should we handle arrays? Like `unique_ptr` with an extent?
+     */
     template <typename Ty, typename OffsetTy>
     class relative_ptr
     {
+        static_assert(std::is_integral_v<OffsetTy>);
+
     public:
         /*
          * std::iterator_traits Type Alias Support
+         *
+         * TODO: Currently `const` is treated as the _this_ pointer is const, not what we're pointing to. I.e. if we
+         * want to change to represent a `const` pointer we'd have to construct a `relative_ptr<const Ty>`. That is, the
+         * `const` does not propagate. We are considering this acceptable
          */
         using difference_type = std::ptrdiff_t; // TODO: Relate to OffsetTy?
         using value_type = Ty;
         using pointer = Ty*;
         using reference = Ty&;
+        using iterator_category = std::random_access_iterator_tag;
+
+
+
+        /*
+         * Constructor(s)/Destructor
+         */
+        relative_ptr() = default;
+
+        relative_ptr(const relative_ptr& ptr) :
+            _offset(calculate_offset(ptr.get()))
+        {
+            assert(get() == ptr.get());
+        }
+
+        template <
+            typename OtherTy,
+            typename OtherOffsetTy,
+            std::enable_if_t<std::is_convertible<OtherTy*, Ty*>::value, int> = 0>
+        relative_ptr(const relative_ptr<OtherTy, OtherOffsetTy>& ptr) :
+            _offset(calculate_offset(ptr.get()))
+        {
+            assert(get() == ptr.get());
+        }
+
+        relative_ptr(std::nullptr_t) noexcept
+        {
+        }
+
+        relative_ptr(pointer ptr) :
+            _offset(calculate_offset(ptr))
+        {
+            assert(get() == ptr);
+        }
+
+
+
+        /*
+         * Modifiers
+         */
+        relative_ptr& operator=(const relative_ptr& ptr)
+        {
+            if (this != &ptr)
+            {
+                reset(ptr.get());
+            }
+
+            return *this;
+        }
+
+        template <
+            typename OtherTy,
+            typename OtherOffsetTy,
+            std::enable_if_t<std::is_convertible<OtherTy*, Ty*>::value, int> = 0>
+        relative_ptr& operator=(const relative_ptr<OtherTy, OtherOffsetTy>& ptr)
+        {
+            reset(ptr.get());
+            return *this;
+        }
+
+        relative_ptr& operator+=(difference_type distance)
+        {
+#if _DEBUG
+            auto expected = get() + distance;
+#endif
+
+            // NOTE: This is meant to mimic pointer arithmetic
+            this->_offset = check_offset(static_cast<difference_type>(this->_offset) + (distance * sizeof(Ty)));
+            assert(get() == expected);
+
+            return *this;
+        }
+
+        relative_ptr& operator-=(difference_type distance)
+        {
+#if _DEBUG
+            auto expected = get() - distance;
+#endif
+
+            // NOTE: This is meant to mimic pointer arithmetic
+            this->_offset = check_offset(static_cast<difference_type>(this->_offset) - (distance * sizeof(Ty)));
+            assert(get() == expected);
+
+            return *this;
+        }
+
+        void reset() noexcept
+        {
+            this->_offset = 0;
+        }
+
+        void reset(std::nullptr_t) noexcept
+        {
+            this->_offset = 0;
+        }
+
+        void reset(pointer ptr)
+        {
+            this->_offset = 0;
+            this->_offset = calculate_offset(ptr);
+            assert(get() == ptr);
+        }
+
+        template <typename OtherOffsetTy>
+        void swap(relative_ptr<Ty, OtherOffsetTy>& other)
+        {
+            auto thisPtr = get();
+            auto otherPtr = other.get();
+
+            // Calculate the offsets before doing the swap so that we have the strong exception guarantee that neither
+            // state is changed if an exception gets thrown
+            auto thisOffset = calculate_offset(otherPtr);
+            auto otherOffset = other.calculate_offset(thisPtr);
+
+            this->_offset = thisOffset;
+            other._offset = otherOffset;
+        }
+
+
+
+        /*
+         * Observers
+         */
+        reference operator*() const noexcept
+        {
+            return *get();
+        }
+
+        pointer operator->() const noexcept
+        {
+            return get();
+        }
+
+        reference operator[](difference_type index) const noexcept
+        {
+            return get()[index];
+        }
+
+        explicit operator bool() const noexcept
+        {
+            return this->_offset != 0;
+        }
+
+        pointer get() const noexcept
+        {
+            if (!this->_offset)
+            {
+                // Pointer to `this` can't possibly point to `Ty`, so we map it to `nullptr`
+                return nullptr;
+            }
+
+            // The behavior of `relative_ptr` is intended to mimic raw pointers. That is, if `this` is `const`, then we
+            // should behave like a `const` pointer (i.e. `Ty* const`) as opposed to a pointer to `const` (i.e.
+            // `const Ty*`), hence the `const_cast`.
+            auto bytePtr = const_cast<std::uint8_t*>(reinterpret_cast<const std::uint8_t*>(this));
+            bytePtr += this->_offset;
+            return reinterpret_cast<pointer>(bytePtr);
+        }
 
 
 
     private:
 
-        Ty& get() noexcept
+        OffsetTy check_offset(std::ptrdiff_t offset) const
         {
-            auto bytePtr = reinterpret_cast<std::uint8_t*>(this);
-            bytePtr += this->_offset;
-            return *reinterpret_cast<Ty*>(bytePtr);
+            auto result = static_cast<OffsetTy>(offset);
+            if (offset != static_cast<std::ptrdiff_t>(result))
+            {
+                throw std::range_error("Pointer offset too large to be represented by relative_ptr");
+            }
+
+            return result;
         }
 
-        const Ty& get() const noexcept
+        OffsetTy calculate_offset(pointer ptr) const
         {
-            auto bytePtr = reinterpret_cast<const std::uint8_t*>(this);
-            bytePtr += this->_offset;
-            return *reinterpret_cast<Ty*>(bytePtr);
+            if (ptr)
+            {
+                auto diff = reinterpret_cast<const std::uint8_t*>(ptr) - reinterpret_cast<const std::uint8_t*>(this);
+                return check_offset(diff);
+            }
+
+            // We allow representing null as a relative_ptr that "points to itself"
+            return 0;
         }
 
 
 
-        OffsetTy _offset;
+        OffsetTy _offset = 0;
     };
 
 
@@ -65,4 +244,15 @@ namespace dhorn::experimental
 
     template <typename Ty>
     using relative_ptr64 = relative_ptr<Ty, std::int64_t>;
+
+
+
+    /*
+     * ADL Functions
+     */
+    template <typename Ty, typename OffsetTy>
+    void swap(relative_ptr<Ty, OffsetTy>& lhs, relative_ptr<Ty, OffsetTy>& rhs)
+    {
+        lhs.swap(rhs);
+    }
 }
--- tests/RelativePtrTests.cpp
+++ tests/RelativePtrTests.cpp
@@ -8,6 +8,7 @@
 #include "stdafx.h"
 
 #include <dhorn/experimental/relative_ptr.h>
+#include <memory>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -15,5 +16,301 @@ using namespace dhorn::experimental;
 
 namespace dhorn::tests
 {
+    /*
+     * Test Types
+     */
+    struct base
+    {
+    };
 
+    struct derived : base
+    {
+    };
+
+
+
+    TEST_CLASS(RelativePtrTests)
+    {
+        TEST_METHOD(SizeTest)
+        {
+            Assert::AreEqual(1u, sizeof(relative_ptr8<int>));
+            Assert::AreEqual(2u, sizeof(relative_ptr16<int>));
+            Assert::AreEqual(4u, sizeof(relative_ptr32<int>));
+            Assert::AreEqual(8u, sizeof(relative_ptr64<int>));
+        }
+
+
+
+#pragma region Constructor Tests
+
+        TEST_METHOD(DefaultConstructorTest)
+        {
+            relative_ptr8<int> ptr;
+            Assert::IsFalse(static_cast<bool>(ptr));
+            Assert::IsNull(ptr.get());
+        }
+
+        TEST_METHOD(NullPtrConstructorTest)
+        {
+            relative_ptr8<int> ptr(nullptr);
+            Assert::IsFalse(static_cast<bool>(ptr));
+            Assert::IsNull(ptr.get());
+        }
+
+        TEST_METHOD(PointerConstructorTest)
+        {
+            int value = 42;
+            relative_ptr8<int> ptr(&value);
+            Assert::IsTrue(static_cast<bool>(ptr));
+            Assert::IsNotNull(ptr.get());
+            Assert::AreEqual(&value, ptr.get());
+            Assert::AreEqual(42, *ptr.get());
+
+            int* intPtr = nullptr;
+            relative_ptr8<int> ptr2(intPtr);
+            Assert::IsFalse(static_cast<bool>(ptr2));
+            Assert::IsNull(ptr2.get());
+        }
+
+        TEST_METHOD(PointerConstructorOutOfRangeTest)
+        {
+            try
+            {
+                int values[256];
+                relative_ptr8<int> ptr(&values[127]);
+                Assert::Fail(L"Expected an exception");
+            }
+            catch (std::range_error&)
+            {
+            }
+        }
+
+        TEST_METHOD(CopyConstructorTest)
+        {
+            int value = 42;
+            relative_ptr8<int> ptr(&value);
+            relative_ptr8<int> ptrCopy(ptr);
+            Assert::IsTrue(static_cast<bool>(ptrCopy));
+            Assert::IsNotNull(ptrCopy.get());
+            Assert::AreEqual(&value, ptrCopy.get());
+            Assert::AreEqual(42, *ptrCopy.get());
+
+            relative_ptr8<int> nullPtr;
+            relative_ptr8<int> nullPtrCopy(nullPtr);
+            Assert::IsFalse(static_cast<bool>(nullPtrCopy));
+            Assert::IsNull(nullPtrCopy.get());
+        }
+
+        TEST_METHOD(CopyConstructorOutOfRangeTest)
+        {
+            int value = 42;
+            relative_ptr8<int> ptr(&value);
+            try
+            {
+                // The heap should be far enough away from the stack...
+                std::make_unique<relative_ptr8<int>>(ptr);
+                Assert::Fail(L"Expected an exception");
+            }
+            catch (std::range_error&)
+            {
+            }
+        }
+
+        TEST_METHOD(MoveConstructorTest)
+        {
+            int value = 42;
+            relative_ptr8<int> ptr(&value);
+            relative_ptr8<int> ptrCopy(std::move(ptr));
+            Assert::IsTrue(static_cast<bool>(ptrCopy));
+            Assert::IsNotNull(ptrCopy.get());
+            Assert::AreEqual(&value, ptrCopy.get());
+            Assert::AreEqual(42, *ptrCopy.get());
+
+            relative_ptr8<int> nullPtr;
+            relative_ptr8<int> nullPtrCopy(std::move(nullPtr));
+            Assert::IsFalse(static_cast<bool>(nullPtrCopy));
+            Assert::IsNull(nullPtrCopy.get());
+        }
+
+        TEST_METHOD(ConversionConstructorTest)
+        {
+            // Can down-cast; cannot up-cast
+            derived d;
+            relative_ptr8<derived> derivedPtr(&d);
+            relative_ptr8<base> basePtr(derivedPtr);
+            Assert::IsTrue(derivedPtr.get() == basePtr.get());
+
+            Assert::IsTrue(std::is_constructible_v<relative_ptr8<base>, relative_ptr8<derived>>);
+            Assert::IsFalse(std::is_constructible_v<relative_ptr8<derived>, relative_ptr8<base>>);
+
+            // Can also add const; cannot remove const
+            relative_ptr8<const base> constBasePtr(derivedPtr);
+            Assert::IsTrue(derivedPtr.get() == constBasePtr.get());
+
+            Assert::IsTrue(std::is_constructible_v<relative_ptr8<const int>, relative_ptr8<int>>);
+            Assert::IsFalse(std::is_constructible_v<relative_ptr8<int>, relative_ptr8<const int>>);
+
+            // Can also change the offset size
+            relative_ptr16<base> basePtr16(derivedPtr);
+            Assert::IsTrue(derivedPtr.get() == basePtr16.get());
+
+            Assert::IsTrue(std::is_constructible_v<relative_ptr16<int>, relative_ptr8<int>>);
+            Assert::IsTrue(std::is_constructible_v<relative_ptr8<int>, relative_ptr16<int>>);
+        }
+
+        TEST_METHOD(ConversionConstructionOutOfRangeTest)
+        {
+            int array[256];
+            relative_ptr16<int> ptr16(&array[127]);
+            try
+            {
+                relative_ptr8<int> ptr8(ptr16);
+                Assert::Fail(L"Expected an exception");
+            }
+            catch (std::range_error&)
+            {
+            }
+        }
+
+#pragma endregion
+
+
+
+#pragma region Assignment Operator Tests
+
+        TEST_METHOD(NullPtrAssignmentTest)
+        {
+            int value;
+            relative_ptr8<int> ptr(&value);
+
+            ptr = nullptr;
+            Assert::IsFalse(static_cast<bool>(ptr));
+            Assert::IsNull(ptr.get());
+        }
+
+        TEST_METHOD(CopyAssignmentTest)
+        {
+            int value = 42;
+            relative_ptr8<int> ptr(&value);
+            relative_ptr8<int> ptrCopy;
+            ptrCopy = ptr;
+
+            Assert::IsTrue(static_cast<bool>(ptrCopy));
+            Assert::IsNotNull(ptrCopy.get());
+            Assert::AreEqual(&value, ptrCopy.get());
+            Assert::AreEqual(42, *ptrCopy.get());
+
+            relative_ptr8<int> nullPtr;
+            relative_ptr8<int> nullPtrCopy;
+            nullPtrCopy = nullPtr;
+
+            Assert::IsFalse(static_cast<bool>(nullPtrCopy));
+            Assert::IsNull(nullPtrCopy.get());
+        }
+
+        TEST_METHOD(CopyAssignmentOutOfRangeTest)
+        {
+            int value = 42;
+            relative_ptr8<int> ptr(&value);
+            try
+            {
+                // The heap should be far enough away from the stack...
+                auto heapPtr = std::make_unique<relative_ptr8<int>>();
+                *heapPtr = ptr;
+                Assert::Fail(L"Expected an exception");
+            }
+            catch (std::range_error&)
+            {
+            }
+        }
+
+        TEST_METHOD(MoveAssignmentTest)
+        {
+            int value = 42;
+            relative_ptr8<int> ptr(&value);
+            relative_ptr8<int> ptrCopy;
+            ptrCopy = std::move(ptr);
+
+            Assert::IsTrue(static_cast<bool>(ptrCopy));
+            Assert::IsNotNull(ptrCopy.get());
+            Assert::AreEqual(&value, ptrCopy.get());
+            Assert::AreEqual(42, *ptrCopy.get());
+
+            relative_ptr8<int> nullPtr;
+            relative_ptr8<int> nullPtrCopy;
+            nullPtrCopy = std::move(nullPtr);
+
+            Assert::IsFalse(static_cast<bool>(nullPtrCopy));
+            Assert::IsNull(nullPtrCopy.get());
+        }
+
+        TEST_METHOD(ConversionAssignmentTest)
+        {
+            // Can down-cast; cannot up-cast
+            derived d;
+            relative_ptr8<derived> derivedPtr(&d);
+            relative_ptr8<base> basePtr;
+            basePtr = derivedPtr;
+
+            Assert::IsTrue(derivedPtr.get() == basePtr.get());
+            Assert::IsTrue(std::is_assignable_v<relative_ptr8<base>&, relative_ptr8<derived>>);
+            Assert::IsFalse(std::is_assignable_v<relative_ptr8<derived>&, relative_ptr8<base>>);
+
+            // Can also add const; cannot remove const
+            relative_ptr8<const base> constBasePtr;
+            constBasePtr = derivedPtr;
+
+            Assert::IsTrue(derivedPtr.get() == constBasePtr.get());
+            Assert::IsTrue(std::is_assignable_v<relative_ptr8<const int>&, relative_ptr8<int>>);
+            Assert::IsFalse(std::is_assignable_v<relative_ptr8<int>&, relative_ptr8<const int>>);
+
+            // Can also change the offset size
+            relative_ptr16<base> basePtr16;
+            basePtr16 = derivedPtr;
+
+            Assert::IsTrue(derivedPtr.get() == basePtr16.get());
+            Assert::IsTrue(std::is_assignable_v<relative_ptr16<int>&, relative_ptr8<int>>);
+            Assert::IsTrue(std::is_assignable_v<relative_ptr8<int>&, relative_ptr16<int>>);
+        }
+
+        TEST_METHOD(ConversionAssignmentOutOfRangeTest)
+        {
+            int array[256];
+            relative_ptr16<int> ptr16(&array[127]);
+            try
+            {
+                relative_ptr8<int> ptr8;
+                ptr8 = ptr16;
+                Assert::Fail(L"Expected an exception");
+            }
+            catch (std::range_error&)
+            {
+            }
+        }
+
+#pragma endregion
+
+
+
+#pragma region Arithmetic Tests
+
+        TEST_METHOD(AdditionAssignmentTest)
+        {
+            int values[] = { 0, 1, 2, 3 };
+            relative_ptr8<int> ptr = values;
+
+            Assert::AreEqual(values[0], *ptr.get());
+
+            ptr += 1;
+            Assert::AreEqual(values[1], *ptr.get());
+
+            ptr += 2;
+            Assert::AreEqual(values[3], *ptr.get());
+
+            ptr += -1;
+            Assert::AreEqual(values[2], *ptr.get());
+        }
+
+#pragma endregion
+    };
 }
